78bbce10a713d304d437188eb10da4825cc6c719
KyowonJeong <jeong@informatik.uni-tuebingen.de>
2022-11-24 20:57:42 +0100

    multiple target files are working `:

--- src/Flash/IDA/Parameter.cs
+++ src/Flash/IDA/Parameter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Xml.Serialization;
 
 namespace Flash.IDA
@@ -7,7 +8,7 @@ namespace Flash.IDA
     /// Parameters for FLASHIda
     /// </summary>
     public class IDAParameters
-    {
+    {        
         public int MaxMs2CountPerMs1 { set; get; }
 
         public int TargetMode { set; get; } 
@@ -21,7 +22,7 @@ namespace Flash.IDA
 
         public double MaxMass { set; get; }
 
-        public string TargetLog { set; get; }
+        public List<string> TargetLogs { set; get; }
 
         [XmlArray()]
         public double[] Tolerances { set; get; }
@@ -39,11 +40,10 @@ namespace Flash.IDA
         /// <param name="maxCharge">Maximal precursor charge</param>
         /// <param name="minMass">Minimal precursor mass</param>
         /// <param name="maxMass">Maximal precursor mass</param> 
-        /// <param name="targetLog">log file containing target or excluded masses</param> 
+        /// <param name="targetLogs">log files containing target or excluded masses</param> 
         /// <param name="targetMode">If set to 1, inclusive targeted mode if 2, exclusive targeted mode. If 0, normal exclusion list mode</param> 
         public IDAParameters(double[] tolerances = null, int maxMs2CountPerMs1 = 5, double qScoreThreshold = -1,
-            double rtWindow = 5, int minCharge = 1, int maxCharge = 100, double minMass = 50, double maxMass = 100000, string targetLog = "NONE"
-            , int targetMode = 0)
+            double rtWindow = 5, int minCharge = 1, int maxCharge = 100, double minMass = 50, double maxMass = 100000, List<string> targetLogs = null, int targetMode = 0)
         {
             Tolerances = tolerances ?? new double[] { 10, 10 };
             RTWindow = rtWindow;
@@ -53,7 +53,7 @@ namespace Flash.IDA
             MinMass = minMass;
             MaxMass = maxMass;
             QScoreThreshold = qScoreThreshold;
-            TargetLog = targetLog;
+            TargetLogs = targetLogs;
             TargetMode = targetMode;
         }
 
@@ -71,8 +71,14 @@ namespace Flash.IDA
         /// <returns></returns>
         public string ToFLASHDeconvInput()
         {
-            return String.Format("max_mass_count {0} score_threshold {1} min_charge {2} max_charge {3} min_mass {4} max_mass {5} RT_window {6} tol {7} target_mode {8} {9}",
-                MaxMs2CountPerMs1, QScoreThreshold, MinCharge, MaxCharge, MinMass, MaxMass, RTWindow, String.Join(" ", Tolerances), TargetMode, TargetLog);
+            var ret = String.Format("max_mass_count {0} score_threshold {1} min_charge {2} max_charge {3} min_mass {4} max_mass {5} RT_window {6} tol {7} target_mode {8} ",
+                MaxMs2CountPerMs1, QScoreThreshold, MinCharge, MaxCharge, MinMass, MaxMass, RTWindow, String.Join(" ", Tolerances), TargetMode);
+
+            foreach(var f in TargetLogs)
+            {
+                ret += f + " ";
+            }
+            return ret;
         }
 
     }
